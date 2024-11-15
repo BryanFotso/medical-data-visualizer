7051fde890e8a753d4a0fe5448362ef11d07755c
Chris-Fotso <chrisfotso@icloud.com>
2024-11-15 15:35:19 +0100

    Add comments

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -3,20 +3,20 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-
+# Import data
 df = pd.read_csv('medical_examination.csv')
 
-
+# Add 'overweight' column
 df1 = df['weight'] / ((df['height'] /100) ** 2)
 df['overweight'] = (df1>25).astype(int)
 
-
+# Normalize data by making 0 always good and 1 always bad. If the value of 'cholesterol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
 df.loc[df['cholesterol'] == 1,'cholesterol'] = 0
 df.loc[df['cholesterol'] > 1,'cholesterol'] = 1
 df.loc[df['gluc'] == 1,'gluc'] = 0
 df.loc[df['gluc'] > 1,'gluc'] = 1
 
-
+# Draw Categorical Plot
 def draw_cat_plot():
     
     df_cat = pd.melt(
@@ -25,7 +25,7 @@ def draw_cat_plot():
         value_vars=["cholesterol", "gluc", "smoke", "alco", "active", "overweight"]
     )
     
-    
+     # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the collumns for the catplot to work correctly.
     df_cat = df_cat.groupby(["cardio", "variable", "value"], as_index=False).size()
     
 
@@ -33,7 +33,7 @@ def draw_cat_plot():
     df_cat.rename(columns={"size": "total"}, inplace=True)
 
 
-   
+   # Draw the catplot with 'sns.catplot()'
     fig = sns.catplot(
         x="variable",
         y="total",
@@ -50,13 +50,14 @@ def draw_cat_plot():
     return fig
 
 
-
+# Draw Heat Map
 def draw_heat_map():
     
     df_heat = df[(df['ap_lo'] <= df['ap_hi']) & (df['height'] >= df['height'].quantile(0.025)) & (df['height'] <= df['height'].quantile(0.975)) & (df['weight'] >= df['weight'].quantile(0.025)) & (df['weight'] <= df['weight'].quantile(0.975)) ]
 
     corr = df_heat.corr()
 
+    # Generate a mask for the upper triangle
     mask = np.triu(np.ones_like(corr, dtype=bool))
 
     fig = plt.figure(figsize=(10, 8))
