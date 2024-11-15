1c2b565ce979a66b6d0750009000df17c43364ed
Chris-Fotso <chrisfotso@icloud.com>
2024-11-15 15:31:32 +0100

    Calculate correlation matrix and plot heatmap

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -3,58 +3,65 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-# 1
-df = None
 
-# 2
-df['overweight'] = None
-
-# 3
+df = pd.read_csv('medical_examination.csv')
 
 
-# 4
+df1 = df['weight'] / ((df['height'] /100) ** 2)
+df['overweight'] = (df1>25).astype(int)
+
+
+df.loc[df['cholesterol'] == 1,'cholesterol'] = 0
+df.loc[df['cholesterol'] > 1,'cholesterol'] = 1
+df.loc[df['gluc'] == 1,'gluc'] = 0
+df.loc[df['gluc'] > 1,'gluc'] = 1
+
+
 def draw_cat_plot():
-    # 5
-    df_cat = None
-
-
-    # 6
-    df_cat = None
+    
+    df_cat = pd.melt(
+        df,
+        id_vars=["cardio"],
+        value_vars=["cholesterol", "gluc", "smoke", "alco", "active", "overweight"]
+    )
+    
+    
+    df_cat = df_cat.groupby(["cardio", "variable", "value"], as_index=False).size()
     
 
-    # 7
+    
+    df_cat.rename(columns={"size": "total"}, inplace=True)
 
 
+   
+    fig = sns.catplot(
+        x="variable",
+        y="total",
+        hue="value",
+        col="cardio",
+        data=df_cat,
+        kind="bar",
+        height=5,
+        aspect=1.2
+    ).fig
 
-    # 8
-    fig = None
-
-
-    # 9
+    
     fig.savefig('catplot.png')
     return fig
 
 
-# 10
+
 def draw_heat_map():
-    # 11
-    df_heat = None
+    
+    df_heat = df[(df['ap_lo'] <= df['ap_hi']) & (df['height'] >= df['height'].quantile(0.025)) & (df['height'] <= df['height'].quantile(0.975)) & (df['weight'] >= df['weight'].quantile(0.025)) & (df['weight'] <= df['weight'].quantile(0.975)) ]
 
-    # 12
-    corr = None
+    corr = df_heat.corr()
 
-    # 13
-    mask = None
+    mask = np.triu(np.ones_like(corr, dtype=bool))
 
+    fig = plt.figure(figsize=(10, 8))
 
-
-    # 14
-    fig, ax = None
-
-    # 15
-
-
-
-    # 16
+    sns.heatmap(corr, annot=True, fmt=".1f", cmap='coolwarm', mask=mask, cbar_kws={'shrink': 0.8})
+    
     fig.savefig('heatmap.png')
     return fig
